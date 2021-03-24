95a24581f410a6d529d961858b0b86405f168192
sergye <sergye@gmail.com>
2021-03-24 08:25:54 +0200

    PlayerTest created

--- tst/io/hexlet/xo/model/PlayerTest.java
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -9,24 +9,22 @@ public class PlayerTest {
     @Test
     public void testGetName() throws Exception{
         final String inputValue = "Slava";
-        final String expectedValue = inputValue;
 
         final Player player = new Player(inputValue, null);
 
         final String actualValue = player.getName();
 
-        assertEquals(expectedValue, actualValue);
+        assertEquals(inputValue, actualValue);
     }
 
     @Test
     public void testGetFigure() throws Exception {
         final Figure inputValue = Figure.X;
-        final Figure expectedValue = inputValue;
 
         final Player player = new Player(null, inputValue);
 
         final Figure actualValue = player.getFigure();
 
-        assertEquals(expectedValue, actualValue);
+        assertEquals(inputValue, actualValue);
     }
 }
