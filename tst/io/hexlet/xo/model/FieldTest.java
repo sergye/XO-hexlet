4e62768238ec4d793421eafe9b3af0b43cff354c
sergye <sergye@gmail.com>
2021-03-24 17:55:10 +0200

    FieldTests checks for exceptions add

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -18,10 +18,6 @@ public class FieldTest {
     }
 
     @Test
-    public void testGetFigure() {
-    }
-
-    @Test
     public void testSetFigure() throws InvalidPointException, AlreadyOccupiedException {
         final Field field = new Field();
 
@@ -60,7 +56,70 @@ public class FieldTest {
         } catch (InvalidPointException e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    public void testGetFigureWhenYIsLessThanZero() throws InvalidPointException, AlreadyOccupiedException {
+        final Field field = new Field();
+
+        final Point inputPoint = new Point(0, -1);
+
+        final Figure actualFigure;
+        try {
+            actualFigure = field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetFigureWhenXIsMoreThanSize() throws InvalidPointException, AlreadyOccupiedException {
+        final Field field = new Field();
+
+        final Point inputPoint = new Point(field.getSize() + 1, 0);
+
+        final Figure actualFigure;
+        try {
+            actualFigure = field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetFigureWhenYIsMoreThanSize() throws InvalidPointException, AlreadyOccupiedException {
+        final Field field = new Field();
+
+        final Point inputPoint = new Point(0, field.getSize() + 1);
+
+        final Figure actualFigure;
+
+        try {
+            actualFigure = field.getFigure(inputPoint);
+            fail();
+        } catch (InvalidPointException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSetFigureWhenAlreaduOccupied() throws InvalidPointException, AlreadyOccupiedException {
+        final Field field = new Field();
+
+        final Point inputPoint = new Point(0, 0);
+
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (AlreadyOccupiedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
