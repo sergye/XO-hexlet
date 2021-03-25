02e1f1d1fd48f2f2a1718fbff775a91647c80815
sergye <sergye@gmail.com>
2021-03-25 08:45:25 +0200

    Create MoveController

---
+++ src/io/hexlet/xo/controller/MoveController.java
@@ -0,0 +1,17 @@
+package io.hexlet.xo.controller;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class MoveController {
+
+    public void applyFigure(Field field, Figure figure, Point point)
+            throws AlreadyOccupiedException, InvalidPointException {
+        field.setFigure(point, figure);
+    }
+
+}
