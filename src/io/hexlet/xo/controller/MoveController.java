2b1e3da702b36ae6da90ec478121650b5c4356cf
sergye <sergye@gmail.com>
2021-03-25 13:44:39 +0200

    Initial move controller

--- src/io/hexlet/xo/controller/MoveController.java
+++ src/io/hexlet/xo/controller/MoveController.java
@@ -11,6 +11,10 @@ public class MoveController {
 
     public void applyFigure(final Field field, final Point point,  final Figure figure)
             throws AlreadyOccupiedException, InvalidPointException {
+
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
         field.setFigure(point, figure);
     }
 
